3fa73c7a52f356f5c05dc774b1cac923e627c264
cypherix93 <bikram.ghosh93@gmail.com>
2016-12-19 19:22:34 -0500

    Fixed the default state issue.

--- src/client/scripts/angular/core/config/Routes.config.ts
+++ src/client/scripts/angular/core/config/Routes.config.ts
@@ -1,8 +1,11 @@
 import {AppModule} from "../../App.module";
 
 // Configure Angular App Routes
-AppModule.config(function ($locationProvider)
+AppModule.config(function ($locationProvider, $urlRouterProvider)
 {
     $locationProvider.html5Mode(false);
     $locationProvider.hashPrefix("");
+
+    // Default when no state provided
+    $urlRouterProvider.otherwise("/");
 });
--- src/client/scripts/angular/core/config/Startup.config.ts
+++ src/client/scripts/angular/core/config/Startup.config.ts
@@ -1,9 +1,7 @@
 import {AppModule} from "../../App.module";
 
 // Configure Angular App Initialization
-AppModule.run(function ($rootScope, $state)
+AppModule.run(function ($rootScope)
 {
     $rootScope.pageTitle = "Home";
-
-    $state.go("homeState");
 });
