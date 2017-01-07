b662563fdd016a2440ccda57f9969d0ecf0fc0d0
cypherix93 <bikram.ghosh93@gmail.com>
2017-01-06 23:00:43 -0500

    Added ImagesController to serve images from the server.

---
+++ src/server/api/controllers/images/ImagesController.ts
@@ -0,0 +1,30 @@
+import path = require("path");
+import uuid = require("node-uuid");
+import fs = require("fs");
+import {Request, Response} from "express-serve-static-core";
+import {Controller, Get, Res, Req, Param, ContentType, EmptyResultCode} from "routing-controllers";
+import {CONFIG} from "../../../config/Config";
+
+@Controller("/images")
+export class ImagesController
+{
+    @Get("/:imageId")
+    @ContentType("jpeg")
+    @EmptyResultCode(404)
+    public async getImage(@Req() req: Request, @Res() res: Response, @Param("imageId") imageId: string)
+    {
+        var imageFile = `${imageId}`;
+        var imagePath = path.join(CONFIG.settings.images.uploadPath, imageFile);
+
+        try
+        {
+            var buffer = fs.readFileSync(imagePath);
+
+            return res.end(buffer); // will send 200
+        }
+        catch (err)
+        {
+            return null; // will send 404
+        }
+    }
+}
