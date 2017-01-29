c950b07dd3665bf7ee90e3a206b314594fcf2ddd
cypherix93 <bikram.ghosh93@gmail.com>
2017-01-28 19:33:23 -0500

    Added comments into build.cmd.

--- build.cmd
+++ build.cmd
@@ -3,11 +3,13 @@
 cls
 
 :: Install NPM packages
+echo Installing NPM packages...
 cmd /c npm install
 
 cls
 
 :: Build and Package Application
+echo Building and Packaging...
 cmd /c npm run package
 
 :: Quit
